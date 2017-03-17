98b2b354893b88b9e14e900112c64a4430a87ab1
Barnes, Patrick <zz-pbarnes@exchange.fullerton.edu>
2017-03-17 13:02:41 -0700

    hm

--- threadingtest/Program.cs
+++ threadingtest/Program.cs
@@ -8,7 +8,10 @@ using System.Threading.Tasks;
 
 namespace threadingtest
 {
-
+    //events
+    //-1    quit
+    //0     nothing
+    //
     class arr_poker
     {
         int ele;
@@ -24,25 +27,28 @@ namespace threadingtest
             arr[ele] = 99;
         }
     }
+    //https://msdn.microsoft.com/en-us/library/edzehd2t(v=vs.110).aspx
+    //http://stackoverflow.com/questions/803242/understanding-events-and-event-handlers-in-c-sharp
     class Program
     {
         static int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
         static List<int> lst; //duplicate to test both
-
+        static void WriteTheString(string s)
+        {
+            Console.WriteLine(s);
+        }
         static void Main(string[] args)
         {
             lst = new List<int>(arr); //dupelicate
 
             //arr first
             arr_poker aclass = new arr_poker(ref arr,5);
-
-
-
-
             for(int i = 0; i < arr.Length; ++i)
             {
                 Console.WriteLine(arr[i] + " ");
             }
+
+            
             Console.ReadKey();
         }
     }
